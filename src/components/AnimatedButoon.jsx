72ac83a2f027cfd1fcb89af4798323042c13a079
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2025-02-07 00:31:50 +0530

    7 feb

--- src/components/AnimatedButoon.jsx
+++ src/components/AnimatedButoon.jsx
@@ -6,7 +6,7 @@ const AnimatedButton = () => {
   return (
     <motion.a
       href="https://drive.google.com/file/d/1jtJhlnr6j8W7dfPrULwLJhpg6B8IaJwv/view?usp=sharing"
-       target="_blank"
+      target="_blank"
       className="relative px-8 py-3 bg-gradient-to-r from-pink-700 to-yellow-700 text-white font-bold rounded-full shadow-lg hover:shadow-2xl hover:from-yellow-500 hover:to-pink-500 transition-transform transform hover:scale-105 duration-300 flex items-center justify-center overflow-hidden cursor-pointer"
       whileHover="hover"
     >
